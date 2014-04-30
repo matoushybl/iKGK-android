dd7d2fe512e7dbdac191fbd01e153e57bf3f6217
Matouš Hýbl <hyblmatous@gmail.com>
2014-04-30 17:20:17 +0200

    Add status bar transparency

--- src/main/java/com/mat/hyb/school/isas/MainActivity.java
+++ src/main/java/com/mat/hyb/school/isas/MainActivity.java
@@ -1,17 +1,35 @@
 package com.mat.hyb.school.isas;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsMenu;
+
+@OptionsMenu(R.menu.main)
+@EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= 19) {
+            Window window = getWindow();
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            SystemBarTintManager systemBarTintManager = new SystemBarTintManager(this);
+            systemBarTintManager.setStatusBarTintColor(getResources().getColor(R.color.baseColor));
+            systemBarTintManager.setNavigationBarAlpha(0.0f);
+            systemBarTintManager.setNavigationBarTintColor(getResources().getColor(android.R.color.black));
+            systemBarTintManager.setStatusBarTintEnabled(true);
+            systemBarTintManager.setNavigationBarTintEnabled(true);
+        }
+        getActionBar().setIcon(R.drawable.ic_ab);
     }
 
 }
