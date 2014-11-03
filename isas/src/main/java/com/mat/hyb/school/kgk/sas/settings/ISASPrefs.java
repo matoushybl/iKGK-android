468a1165e2303920a6dc21522c8a1d1623e0b58b
Matouš Hýbl <hyblmatous@gmail.com>
2014-11-03 19:22:51 +0100

    Add new prefs draft, update styles

---
+++ isas/src/main/java/com/mat/hyb/school/kgk/sas/settings/ISASPrefs.java
@@ -0,0 +1,18 @@
+package com.mat.hyb.school.kgk.sas.settings;
+
+import org.androidannotations.annotations.sharedpreferences.SharedPref;
+
+/**
+ * @author <a href="mailto:hyblmatous@gmail.com">Matous Hybl</a>
+ */
+@SharedPref(SharedPref.Scope.UNIQUE)
+public interface ISASPrefs {
+
+    boolean teacherMode();
+
+    int id();
+
+    boolean externalBrowserMode();
+
+    boolean firstRun();
+}
